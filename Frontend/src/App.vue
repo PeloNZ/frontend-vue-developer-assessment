ca88399fe3c44e34419519fa2870054688907528
Chris Wharton <cwharton86@gmail.com>
2024-01-18 13:03:56 +1300

    As a refactoring example, move the todo list to a separate component

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -202,26 +202,7 @@ function orderByStatus() {
             Showing {{ items.length }} Item(s)
             <button type="button" class="pull-right btn btn-primary" @click="getItems">Refresh</button>
           </h1>
-          <table class="table table-striped table-bordered table-hover">
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Description</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in items" :key="item.id">
-                <td>{{ item.id }}</td>
-                <td>{{ item.description }}</td>
-                <td>
-                  <button v-show="item.isCompleted === false" type="button" class="btn btn-warning btn-sm" @click="() => handleMarkAsComplete(item)">
-                    Mark as completed
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <TodoItemsList :items="items" @markAsComplete="handleMarkAsComplete" />
         </div>
       </div>
     </div>
